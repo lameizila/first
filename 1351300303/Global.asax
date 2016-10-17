9c39a91755dfcddd0f4fc11aaf04005d65160eed
stu <stu@qq.com>
2016-10-17 18:00:56 +0800

    添加项目文件。

---
+++ 1351300303/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1351300303.MvcApplication" Language="C#" %>
---
+++ 1351300303/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1351300303.Startup))]
+namespace _1351300303
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
